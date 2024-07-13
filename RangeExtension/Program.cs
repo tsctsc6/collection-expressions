522ae2b1f169106174b47df8d8a5bad96e47ffbb
tsctsc6@126.com <tsctsc6@126.com>
2024-07-13 10:48:56 +0800

    添加项目文件。

---
+++ RangeExtension/Program.cs
@@ -0,0 +1,23 @@
+﻿using RangeExtension.Utilities;
+
+namespace RangeExtension;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        foreach(var i in 0..5)
+        {
+            Console.Write($"{i}, ");
+        }
+        Console.WriteLine();
+        RangeCollention r1 = new([0..5, 10..15]);
+        RangeCollention r2 = RangeCollentionBuilder.Create([0..5, 10..15]);
+        //RangeCollention r3 = [0..5, 10..15];
+        foreach (var i in r1)
+        {
+            Console.Write($"{i}, ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ RangeExtension/Utilities/RangeExtension.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace RangeExtension.Utilities;
+
+public static class RangeExtension
+{
+    public static IEnumerator<int> GetEnumerator(this Range range)
+    {
+        for (int i = range.Start.Value; i < range.End.Value; i++)
+            yield return i;
+    }
+}
+
+[CollectionBuilder(typeof(RangeCollentionBuilder), "Create")]
+public class RangeCollention : IEnumerable<Range>
+{
+    private readonly Range[] ranges;
+
+    public RangeCollention(ReadOnlySpan<Range> ranges)
+    {
+        this.ranges = ranges.ToArray();
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        foreach (var r in ranges)
+        {
+            foreach (var i in r)
+            {
+                yield return i;
+            }
+        }
+    }
+
+    IEnumerator<Range> IEnumerable<Range>.GetEnumerator()
+    {
+        return ((IEnumerable<Range>)ranges).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ranges.GetEnumerator();
+    }
+}
+
+public static class RangeCollentionBuilder
+{
+    public static RangeCollention Create(ReadOnlySpan<Range> values) => new(values);
+}
